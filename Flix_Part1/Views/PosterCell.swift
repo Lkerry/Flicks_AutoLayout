44b336fc705ef1cd04891b80fb9f11e5b472a2a8
Lkerry <annekerrieleveille@yahoo.fr>
2018-09-29 18:49:45 -0400

    First Commit
    First Commit

---
+++ Flix_Part1/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  Flix_Part1
+//
+//  Created by Kerry LEVEILLE on 9/26/18.
+//  Copyright © 2018 Kerry LEVEILLE. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
